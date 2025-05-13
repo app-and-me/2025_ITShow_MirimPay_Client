a0164325d0b2dccef177d7613264756e47c6d735
cuzurmyhabit <cuzurmyhabit>
2025-05-13 19:22:47 +0900

    feat: 결제 수단 선택 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from './pages/test';
 import Cart from './pages/Cart';
+import Payment from "./pages/Payment";
 import Pay from './pages/Pay';
 import Face from './pages/Face';
 import Pin from './pages/Pin';
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
         <Route path="/pay" element={<Pay />} />
         <Route path="/face" element={<Face />} />
         <Route path="/pin" element={<Pin />} />
