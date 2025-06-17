db1cfc9944a35937d7db1b65282f10399ab8fce3
cuzurmyhabit <s2472@e-mirim.hs.kr>
2025-06-18 01:14:28 +0900

    add: 첫 화면 자동 넘김 기능

--- src/pages/Successful.tsx
+++ src/pages/Successful.tsx
@@ -1,32 +1,36 @@
 import Shopimg from '../assets/Shop.png';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PaymentContainer = styled.div`
   padding: 1.5rem;
-  padding-bottom: 15rem;
   margin: 0 auto;
   background-color: #008C0E;
   height: 100vh;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  overflow: hidden;
 `;
 
 const SquareBox = styled.div`
-  margin-top: 40px;
   width: 85%;
+  height: 80vh;
   background-color: #fff;
   border-radius: 20px;
-  padding: 400px 20px 100px 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
+  justify-content: space-between;
+  padding: 20px;
+  
 `;
 
 const Circle = styled.div`
   position: absolute;
-  top: 110px;
+  top: 20%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -36,6 +40,29 @@ const Circle = styled.div`
   background: #0F9444;
 `;
 
+const Title = styled.div`
+  text-align: center;
+  font-size: 32px;
+  color: black;
+  font-weight: bold;
+  margin-top: 75%;
+  line-height: 1.5;
+`;
+
+const CountdownText = styled.div`
+  text-align: center;
+  font-size: 18px;
+  color: #666;
+`;
+
+const SubTitle = styled.div`
+  text-align: center;
+  font-size: 32px;
+  color: #0F9444;
+  font-weight: bold;
+  margin-bottom: 10%;
+`;
+
 const ShopImg = styled.img`
   margin-left: 8px;
   margin-bottom: -4px;
@@ -43,27 +70,31 @@ const ShopImg = styled.img`
   height: 90px;
 `;
 
-const Title = styled.div`
-  text-align: center;
-  font-size: 22px;
-  color: black;
-  font-weight: bold;
-  margin-top: -60px;
-  line-height: 1.5;
-
-`;
-
-const SubTitle = styled.div`
-  text-align: center;
-  font-size: 20px;
-  color: #0F9444;
-  font-weight: bold;
-  margin-top: 300px;
-`;
-
 export default function Payment() {
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    const redirect = setTimeout(() => {
+      navigate('/main');
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+      clearTimeout(redirect);
+    };
+  }, [navigate]);
+
+  const handleClick = () => {
+    navigate('/main');
+  };
+
   return (
-    <PaymentContainer>
+    <PaymentContainer onClick={handleClick}>
       <SquareBox>
         <Circle>
           <ShopImg src={Shopimg} alt="shop" />
@@ -72,6 +103,9 @@ export default function Payment() {
           결제가 성공적으로<br />
           완료되었습니다!
         </Title>
+        <CountdownText>
+          {countdown}초 후에 처음 화면으로 돌아갑니다
+        </CountdownText>
         <SubTitle>MIRIM PAY</SubTitle>
       </SquareBox>
     </PaymentContainer>
